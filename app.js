89dc7bb8d42c33b79ed83c123c965562230b4f30
Thomas Johnson <thomasj.johnson20@gmail.com>
2022-02-28 17:24:25 -0600

    made the links in my nav bar take you to certain id in my page using jquery

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav);
 ////////////////////////////////////////////
+
+//$("#about").click(function(){
+  //  $(document).scrollTop(2000)
+  //  });
+
+$("#about-link").click(function() {
+     $([document.documentElement, document.body]).animate({
+     scrollTop: $("#my-portfolio").offset().top
+     }, 2000);
+});
+$("#project-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+    scrollTop: $("#projects").offset().top
+    }, 2000);
+});
+$("#skills-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+    scrollTop: $("#skills").offset().top
+    }, 2000);
+});
+$("#resume-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+    scrollTop: $("#resume").offset().top
+    }, 2000);
+});
+    
