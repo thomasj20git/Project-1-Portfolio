05b187320bd1287d296350a10258e7057952af1e
Thomas Johnson <thomasj.johnson20@gmail.com>
2022-03-02 11:20:26 -0600

    added accrodian for skilsli want to learn section

--- app.js
+++ app.js
@@ -92,6 +92,25 @@ const closeModal3 = () => {
 $openBtn3.on('click', openModal3);
 $closeBtn3.on('click', closeModal3);
 
+////////////////////////////////////////
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 
 
 
